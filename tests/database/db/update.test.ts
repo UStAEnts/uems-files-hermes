499d940ba43966c83418394deca1a51065feea02
Ryan Delaney <vitineth@gmail.com>
2021-01-14 16:57:01 +0000

    Adding update tests to the module, slowly getting there

---
+++ tests/database/db/update.test.ts
@@ -0,0 +1,144 @@
+import { Db, MongoClient, ObjectId } from "mongodb";
+import { BaseSchema } from "@uems/uemscommlib/build/BaseSchema";
+import { DatabaseFile, FileDatabase } from "../../../src/database/FileDatabase";
+import { GetFileNameFunction, UpdateFunction } from "../../../src/uploader/UploadServer";
+import { FileResponse } from "@uems/uemscommlib";
+import Intentions = BaseSchema.Intentions;
+import { defaultAfterAll, defaultAfterEach, defaultBeforeAll, defaultBeforeEach, haveNoAdditionalKeys } from "../../utilities/setup";
+
+const empty = <T extends Intentions>(intention: T): { msg_intention: T, msg_id: 0, status: 0, userID: string } => ({
+    msg_intention: intention,
+    msg_id: 0,
+    status: 0,
+    userID: 'user',
+})
+
+describe('create messages of states', () => {
+    let client!: MongoClient;
+    let db!: Db;
+
+    let mocks = {
+        setResolver: jest.fn(),
+        generateDownloadURI: jest.fn(),
+        launch: jest.fn(),
+        deleteFile: jest.fn(),
+        provisionUploadURI: jest.fn(),
+    }
+
+    beforeAll(async () => {
+        const { client: newClient, db: newDb } = await defaultBeforeAll();
+        client = newClient;
+        db = newDb;
+
+        fileDB = new FileDatabase(db, { details: 'details', changelog: 'changelog' }, {
+            setResolver(resolver: GetFileNameFunction): void {
+                mocks.setResolver.apply(this, resolver);
+            },
+            generateDownloadURI(file: DatabaseFile): Promise<string> {
+                return Promise.resolve(mocks.generateDownloadURI.apply(this, file));
+            },
+            launch(): Promise<void> {
+                mocks.launch.apply(this);
+                return Promise.resolve();
+            },
+            deleteFile(file: DatabaseFile): Promise<void> {
+                mocks.deleteFile.apply(this, file);
+                return Promise.resolve();
+            },
+            provisionUploadURI(file: FileResponse.InternalFile, update: UpdateFunction): Promise<string> {
+                return Promise.resolve(mocks.provisionUploadURI.apply(this, [file, update]));
+            },
+        });
+    });
+
+    afterAll(() => defaultAfterAll(client, db));
+    beforeEach(async () => {
+        // Fully reset the mocks with new one
+        mocks = {
+            setResolver: jest.fn(),
+            generateDownloadURI: jest.fn(),
+            launch: jest.fn(),
+            deleteFile: jest.fn(),
+            provisionUploadURI: jest.fn(),
+        }
+        await defaultBeforeEach([{
+            _id: new ObjectId('56d9bf92f9be48771d6fe5b0'),
+            name: 'mongoose alex',
+            filename: 'mongoose_alex_alpha',
+            size: 1000,
+            mime: 'text/html',
+            owner: 'alpha_owner',
+            type: 'alpha doc',
+            date: 10345,
+            downloadURL: '/somewhere',
+            filePath: 'fs/data.com',
+            events: ['alpha_event1', 'alpha_event2'],
+        }, {
+            _id: new ObjectId('56d9bf92f9be48771d6fe5b1'),
+            name: 'alfredo shark',
+            filename: 'alfredo_shark_beta',
+            size: 1500,
+            mime: 'text/json',
+            owner: 'beta_owner',
+            type: 'beta doc',
+            date: 10349,
+            downloadURL: '/beta',
+            filePath: 'fs/beta.com',
+            events: ['beta_event1', 'beta_event2'],
+        }], client, db)
+    });
+    afterEach(() => defaultAfterEach(client, db));
+
+    let fileDB: FileDatabase;
+
+    it('should allow basic updates', async () => {
+        const update = await fileDB.update({
+            ...empty('UPDATE'),
+            id: '56d9bf92f9be48771d6fe5b1',
+            name: 'some new file name',
+            type: 'new file type'
+        });
+
+        expect(update).toEqual(['56d9bf92f9be48771d6fe5b1']);
+
+        const query = await fileDB.query({
+            ...empty('READ'),
+            id: '56d9bf92f9be48771d6fe5b1',
+        });
+
+        expect(query).toHaveLength(1);
+        expect(query[0]).toHaveProperty('name', 'some new file name');
+        expect(query[0]).toHaveProperty('filename', 'alfredo_shark_beta');
+        expect(query[0]).toHaveProperty('type', 'new file type');
+    });
+
+    it('should prevent adding additional properties via updates', async () => {
+        const update = await fileDB.update({
+            ...empty('UPDATE'),
+            id: '56d9bf92f9be48771d6fe5b1',
+            name: 'some new file name',
+            type: 'new file type',
+            // @ts-ignore
+            newProp: 'new property',
+        });
+
+        expect(update).toEqual(['56d9bf92f9be48771d6fe5b1']);
+
+        const query = await fileDB.query({
+            ...empty('READ'),
+            id: '56d9bf92f9be48771d6fe5b1',
+        });
+        expect(query).toHaveLength(1);
+        haveNoAdditionalKeys(query[0], [
+            'id',
+            'name',
+            'filename',
+            'size',
+            'mime',
+            'owner',
+            'type',
+            'date',
+            'downloadURL',
+        ]);
+    });
+});
---
+++ tests/database/messaging/update.binding.test.ts
@@ -0,0 +1,193 @@
+import { Db, MongoClient, ObjectId } from "mongodb";
+import { RabbitNetworkHandler } from "@uems/micro-builder";
+import { BaseSchema } from "@uems/uemscommlib/build/BaseSchema";
+import { FileBindingMessage, FileMessage, FileResponse, MsgStatus } from "@uems/uemscommlib";
+import { BindingBroker } from "../../utilities/BindingBroker";
+import { DatabaseFile, FileDatabase } from "../../../src/database/FileDatabase";
+import { defaultAfterAll, defaultAfterEach, defaultBeforeAll, defaultBeforeEach, haveNoAdditionalKeys } from "../../utilities/setup";
+import bind from "../../../src/Binding";
+import { GetFileNameFunction, UpdateFunction } from "../../../src/uploader/UploadServer";
+import Intentions = BaseSchema.Intentions;
+import ReadFileMessage = FileMessage.ReadFileMessage;
+import DeleteFileMessage = FileMessage.DeleteFileMessage;
+import UpdateFileMessage = FileMessage.UpdateFileMessage;
+import CreateFileMessage = FileMessage.CreateFileMessage;
+import UnbindFilesFromEventMessage = FileBindingMessage.UnbindFilesFromEventMessage;
+import UnbindEventsFromFileMessage = FileBindingMessage.UnbindEventsFromFileMessage;
+import BindFilesToEventMessage = FileBindingMessage.BindFilesToEventMessage;
+import BindEventsToFileMessage = FileBindingMessage.BindEventsToFileMessage;
+import QueryByFileMessage = FileBindingMessage.QueryByFileMessage;
+import QueryByEventMessage = FileBindingMessage.QueryByEventMessage;
+import { constants } from "http2";
+
+const empty = <T extends Intentions>(intention: T): { msg_intention: T, msg_id: 0, status: 0, userID: string } => ({
+    msg_intention: intention,
+    msg_id: 0,
+    status: 0,
+    userID: 'user',
+})
+
+describe('create messages of states', () => {
+    let client!: MongoClient;
+    let db!: Db;
+
+    let broker!: BindingBroker<ReadFileMessage | QueryByFileMessage | QueryByEventMessage, DeleteFileMessage | UnbindFilesFromEventMessage | UnbindEventsFromFileMessage, UpdateFileMessage, CreateFileMessage | BindFilesToEventMessage | BindEventsToFileMessage, FileMessage.FileMessage>;
+    let fakeBroker!: RabbitNetworkHandler<any, any, any, any, any, any>;
+
+    let mocks = {
+        setResolver: jest.fn(),
+        generateDownloadURI: jest.fn(),
+        launch: jest.fn(),
+        deleteFile: jest.fn(),
+        provisionUploadURI: jest.fn(),
+    }
+
+    let fileDB: FileDatabase;
+
+    beforeAll(async () => {
+        const { client: newClient, db: newDb } = await defaultBeforeAll();
+        client = newClient;
+        db = newDb;
+
+        broker = new BindingBroker();
+        fakeBroker = broker as unknown as RabbitNetworkHandler<any, any, any, any, any, any>;
+
+        fileDB = new FileDatabase(db, { details: 'details', changelog: 'changelog' }, {
+            setResolver(resolver: GetFileNameFunction): void {
+                mocks.setResolver.apply(this, resolver);
+            },
+            generateDownloadURI(file: DatabaseFile): Promise<string> {
+                return Promise.resolve(mocks.generateDownloadURI.apply(this, file));
+            },
+            launch(): Promise<void> {
+                mocks.launch.apply(this);
+                return Promise.resolve();
+            },
+            deleteFile(file: DatabaseFile): Promise<void> {
+                mocks.deleteFile.apply(this, file);
+                return Promise.resolve();
+            },
+            provisionUploadURI(file: FileResponse.InternalFile, update: UpdateFunction): Promise<string> {
+                return Promise.resolve(mocks.provisionUploadURI.apply(this, [file, update]));
+            },
+        });
+    });
+    afterAll(() => defaultAfterAll(client, db));
+    beforeEach(() => {
+        // Fully reset the mocks with new one
+        mocks = {
+            setResolver: jest.fn(),
+            generateDownloadURI: jest.fn(),
+            launch: jest.fn(),
+            deleteFile: jest.fn(),
+            provisionUploadURI: jest.fn(),
+        }
+
+        broker.clear();
+        bind(fileDB, fakeBroker);
+        defaultBeforeEach([{
+            _id: new ObjectId('56d9bf92f9be48771d6fe5b0'),
+            name: 'mongoose alex',
+            filename: 'mongoose_alex_alpha',
+            size: 1000,
+            mime: 'text/html',
+            owner: 'alpha_owner',
+            type: 'alpha doc',
+            date: 10345,
+            downloadURL: '/somewhere',
+            filePath: 'fs/data.com',
+            events: ['alpha_event1', 'alpha_event2'],
+        }, {
+            _id: new ObjectId('56d9bf92f9be48771d6fe5b1'),
+            name: 'alfredo shark',
+            filename: 'alfredo_shark_beta',
+            size: 1500,
+            mime: 'text/json',
+            owner: 'beta_owner',
+            type: 'beta doc',
+            date: 10349,
+            downloadURL: '/beta',
+            filePath: 'fs/beta.com',
+            events: ['beta_event1', 'beta_event2'],
+        }], client, db)
+    });
+    afterEach(() => defaultAfterEach(client, db));
+
+    it('should allow basic updates', async () => {
+        const update = await broker.promiseEmit('update', {
+            ...empty('UPDATE'),
+            id: '56d9bf92f9be48771d6fe5b1',
+            name: 'some new file name',
+            type: 'new file type'
+        }, 'file.details.update');
+
+        expect(update).toHaveProperty('result');
+        expect(update).toHaveProperty('status');
+        expect(update.status).toEqual(MsgStatus.SUCCESS);
+        expect(update.result).toHaveLength(1);
+        expect(update.result[0]).toEqual('56d9bf92f9be48771d6fe5b1');
+
+        const query = await broker.promiseEmit('query', {
+            ...empty('READ'),
+            id: '56d9bf92f9be48771d6fe5b1',
+        }, 'file.details.read');
+
+        expect(query).toHaveProperty('status', MsgStatus.SUCCESS);
+        expect(query).toHaveProperty('result');
+        expect(query.result).toHaveLength(1);
+        expect(query.result[0]).toHaveProperty('name', 'some new file name');
+        expect(query.result[0]).toHaveProperty('filename', 'alfredo_shark_beta');
+        expect(query.result[0]).toHaveProperty('type', 'new file type');
+    });
+
+    it('should prevent adding additional properties via updates', async () => {
+        // @ts-ignore
+        const update = await broker.promiseEmit('update', {
+            ...empty('UPDATE'),
+            id: '56d9bf92f9be48771d6fe5b1',
+            name: 'abc',
+            newProperty: 'abc'
+        }, 'file.details.update');
+
+        expect(update).toHaveProperty('result');
+        expect(update).toHaveProperty('status');
+        expect(update.status).toEqual(MsgStatus.SUCCESS);
+        expect(update.result).toHaveLength(1);
+        expect(update.result[0]).toEqual('56d9bf92f9be48771d6fe5b1');
+
+        const query = await broker.promiseEmit('query', {
+            ...empty('READ'),
+            id: '56d9bf92f9be48771d6fe5b1',
+        }, 'file.details.read');
+
+        expect(query).toHaveProperty('status', MsgStatus.SUCCESS);
+        expect(query).toHaveProperty('result');
+        expect(query.result).toHaveLength(1);
+        haveNoAdditionalKeys(query.result[0], [
+            'id',
+            'name',
+            'filename',
+            'size',
+            'mime',
+            'owner',
+            'type',
+            'date',
+            'downloadURL',
+        ]);
+    });
+
+    it('should prevent updates with zero operations', async () => {
+        const update = await broker.promiseEmit('update', {
+            ...empty('UPDATE'),
+            id: '56d9bf92f9be48771d6fe5b1',
+        }, 'file.details.update');
+
+        expect(update).toHaveProperty('result');
+        expect(update).toHaveProperty('status');
+        expect(update.status).not.toEqual(MsgStatus.SUCCESS);
+        expect(update.status).not.toEqual(constants.HTTP_STATUS_INTERNAL_SERVER_ERROR);
+        expect(update.result).toHaveLength(1);
+        expect(update.result[0]).toContain('operation');
+    });
+
+});
